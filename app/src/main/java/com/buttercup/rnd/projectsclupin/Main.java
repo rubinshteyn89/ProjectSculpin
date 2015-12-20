321ca371ffa66b24dce748f06f6236b53e0420e0
Ilya Rubinshteyn <ilya_rubinshteyn@Ilya.local>
2015-12-19 21:23:21 -0800

    Fixed everything :)

--- app/src/main/java/com/buttercup/rnd/projectsclupin/Main.java
+++ app/src/main/java/com/buttercup/rnd/projectsclupin/Main.java
@@ -1,6 +1,11 @@
 package com.buttercup.rnd.projectsclupin;
 
 import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.os.Bundle;
 
 public class Main extends ActionBarActivity {
@@ -9,5 +14,25 @@ public class Main extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        // Inflate the menu;
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        int id = item.getItemId();
+
+
+        if (id == R.id.action_settings) {
+            return true;
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
